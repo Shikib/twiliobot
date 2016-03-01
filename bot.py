0077dfc2c1451243839bda4d05e6692d3d70391c
Shikib Mehri <mehrishikib@gmail.com>
2016-03-01 10:12:26 -0800

    implemented text functionality and half-finished call functionality

--- bot.py
+++ bot.py
@@ -1,19 +1,53 @@
 from telegram import Updater
+from twilio.rest import TwilioRestClient
 
-updater = Updater(token="INSERT_YOUR_TOKEN_HERE")
+TELEGRAM_TOKEN = "INSERT_YOUR_TOKEN_HERE"
+TWILIO_ACCOUNT_SID = "INSERT_ACCOUNT_SID_HERE"
+TWILIO_AUTH_TOKEN = "INSERT_AUTH_TOKEN_HERE"
 
+# initialize telegram updater and dispatcher
+updater = Updater(token=TELEGRAM_TOKEN)
 dispatcher = updater.dispatcher
 
+# initialize twilio client
+client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
 def call(bot, chat_id, cmd_text):
+  # set source_number to be a number associated with the account
+  source_number = "+18668675309"
+
+  # cmd_text should be in format [Phone Number] [Message]
+  cmd_words = cmd_text.split()
+  phone_number = cmd_words[0]
+  message_text = cmd_words[1:] if len(cmd_words) > 1 else "Yes"
+
   # Use TWILIO's API to call based on the cmd_text
+  call = client.calls.create(
+          url="http://demo.twilio.com/docs/voice.xml",
+          to=phone_number,
+          from=source_number)
+
   # Send a confirmation message to the issuer of the command
-  msg = "My call method has not been implemented yet. Sorry!"
+  msg = "Sent a call to the phone number with the default message."
   bot.sendMessage(chat_id=chat_id, text=msg)
 
 def text(bot, chat_id, cmd_text):
+  # set source_number to be a number associated with the account
+  source_number = "+18668675309"
+
+  # cmd_text should be in format [Phone Number] [Message]
+  cmd_words = cmd_text.split()
+  phone_number = cmd_words[0]
+  message_text = cmd_words[1:] if len(cmd_words) > 1 else "Yes"
+
   # Use TWILIO's API to text based on the cmd_text
+  call = client.calls.create(
+          to=phone_number,
+          from=source_number,
+          body=message_text)
+
   # Send a confirmation message to the issuer of the command
-  msg = "My text method has not been implemented yet. Sorry!"
+  msg = "Sent a text to the phone number with your message."
   bot.sendMessage(chat_id=chat_id, text=msg)
 
 def main(bot, update):
